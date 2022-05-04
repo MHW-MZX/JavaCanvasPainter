a3666afca92cdc5144b8ea9aa79072f7c86cf482
Malik Zurkiyeh <malik.zurkiyeh@gmail.com>
2022-05-04 18:29:53 -0400

    added few features still WIP

--- src/Controller.java
+++ src/Controller.java
@@ -4,18 +4,27 @@ import java.awt.event.MouseListener;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.awt.Graphics;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.text.AttributeSet.ColorAttribute;
+
 import java.awt.Color;
 import java.awt.event.MouseMotionListener;
-public class Controller implements MouseListener, MouseMotionListener {
+public class Controller extends View implements MouseListener, MouseMotionListener {
 	int mouseX, mouseY;
-	boolean isPressed;
-	MouseEvent mE;
+	Graphics g;
+	JLabel[] buttonLabels = getLabelSet();
+	String[] labelNameStrings = getLabelNameArray();
 	public Controller(View v) {
 		System.out.println("inside controller");
 		v.setupDisplay();
+		setLabelVisibility(buttonLabels);
 		View.panel.addMouseListener(this);
 		View.panel.addMouseMotionListener(this);
+		for(int i = 0; i < buttonLabels.length; i++)
+			buttonLabels[i].addMouseListener(this);
 	}
 	
 	public Controller() {
@@ -24,52 +33,59 @@ public class Controller implements MouseListener, MouseMotionListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		mouseDragged(e);
+		for(int i = 0; i < buttonLabels.length; i++) {
+			if(e.getSource() == buttonLabels[i]) {
+				System.out.printf("You clicked: %s\n", labelNameStrings[i]);
+			}
+		}
 		//View.panel.repaint();
 		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
+		if(e.getSource() == View.panel) {
+		g = View.panel.getGraphics();
+		g.setColor(Color.RED);
+		g.fillOval(e.getX() - 15, e.getY() - 20,30,30);
+		System.out.println("Test");
+		mouseDragged(e);
+		}
 		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		isPressed = false;
 		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < buttonLabels.length; i++) {
+			buttonLabels[i].setOpaque(true);
+			if(e.getSource() == buttonLabels[i]) {
+				buttonLabels[i].setBackground(Color.GRAY);
+			}
+		}
 		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < buttonLabels.length; i++) {
+			if(e.getSource() == buttonLabels[i] && buttonLabels[i].getBackground() == Color.GRAY) 
+				buttonLabels[i].setBackground(new JButton().getBackground());
+		}
 		
 	}
 	
-	public void paint(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.drawOval(200,200,30,30);
-	}
 
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		
-		Graphics g = View.panel.getGraphics();
+		g = View.panel.getGraphics();
 		g.setColor(Color.RED);
-		isPressed = true;
-		g.fillOval(e.getX() - 10, e.getY() - 20,30,30);
+		g.fillOval(e.getX() - 15, e.getY() - 20,30,30);
 		System.out.println("Test");
 	}
 
@@ -78,5 +94,10 @@ public class Controller implements MouseListener, MouseMotionListener {
 		// TODO Auto-generated method stub
 		
 		
+	}
+	
+	private void setLabelVisibility(JLabel[] labelArray) {
+		for(int i = 0; i < labelArray.length; i++) 
+			labelArray[i].setOpaque(true);
 	}
 }
--- src/View.java
+++ src/View.java
@@ -1,36 +1,130 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
 
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.plaf.PanelUI;
+
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
-public class View extends JFrame {
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	double width = screenSize.getWidth();
-	double height = screenSize.getHeight();
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import org.kordamp.ikonli.swing.FontIcon;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+
+public class View extends JFrame implements MouseListener {
+	private static int imgNum = new File("src//imgs").listFiles().length;
 	public static JPanel panel = new JPanel();
-	JPanel buttonsPanel = new JPanel();
+	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private double width = screenSize.getWidth();
+	private double height = screenSize.getHeight();
+	private JPanel buttonsPanel = new JPanel();
+	private static JLabel [] toolsButtonSetIcons = new JLabel[imgNum];
+	private JMenuBar menuBar = new JMenuBar();
+	private ImageIcon[] icons = new ImageIcon[imgNum];
+	private URL [] iconURLs = new URL[imgNum];
+	private JMenu menu = new JMenu("Menu");
+	private GridBagConstraints gbc = new GridBagConstraints();
+	private static String[] toolNameStrings = {"Eraser","Cutter","Pencil","Color Chooser","Color Picker","Transform","Picture"};
+	
+	
 	
 	public View() {	
+		
 	}
-	//asdf
+	
 	public void setupDisplay() {
-		this.setSize(new Dimension((int) width / 2, (int) height / 2));
+		this.setSize(new Dimension((int) width - 20, (int) height - 60 ));
 		this.setTitle("Canvas Painter");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.add(panel);
-		this.add(buttonsPanel,BorderLayout.SOUTH);
-		buttonsPanel.setLayout(new FlowLayout());
-		buttonsPanel.add(new JButton());
+		this.add(buttonsPanel,BorderLayout.WEST);
+		this.setJMenuBar(menuBar);
+		menuBar.add(menu);
+		buttonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		setupToolButtonSet();
 		System.out.printf("JFrame width: %d and height: %d ", this.getWidth(), this.getHeight());
+		this.setVisible(true);
 	}
 	
+	private void setupToolButtonSet() {
+		buttonsPanel.setPreferredSize(new Dimension(150,100));
+		
+		System.out.println(imgNum);
+		for(int i = 0 ; i < imgNum; i++) {
+			//buttonsPanel.add(Box.createRigidArea(new Dimension(5, 35)));
+			iconURLs[i] = getClass().getResource(String.format("./imgs/icon%d.png",i));
+			System.out.println(String.format("./imgs/icon%d.png",i));
+			icons[i] = new ImageIcon(new ImageIcon(iconURLs[i]).getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
+			toolsButtonSetIcons[i] = new JLabel(icons[i]);
+			buttonsPanel.add(toolsButtonSetIcons[i]);
+
+			
+		}
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+	
+	protected static JLabel[] getLabelSet() {
+		return toolsButtonSetIcons;
+	}
+	
+	protected static String[] getLabelNameArray() {
+		return toolNameStrings;
+	}
 	
 }
+
